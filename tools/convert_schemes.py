c438a5bf402ceac286596355aa00800914d25a7f
alopex <alopex.fduch.org@gmail.com>
2026-01-17 23:12:03 +0200

    scheme preview is now 720px wide generate keywords for package.json

--- tools/convert_schemes.py
+++ tools/convert_schemes.py
@@ -3,13 +3,15 @@ from spectrum.main import generate_pixels, parse_hex
 from PIL import Image
 # from typing import NoReturn
 
-import pprint
+# import pprint
 import yaml
 import json
+import re
 
 source_path = "../schemes/base16"
 schemes_path = "../styles/schemes"
 settings_path = "../lib/base16c_settings.json"
+package_path = "../package.json"
 
 def lower_filename(name: str) -> str:
     """Convert scheme name to file name."""
@@ -18,6 +20,27 @@ def lower_filename(name: str) -> str:
         .replace(",", "")\
         .replace("é", "e")\
         .replace(" ", "-").lower()
+
+def lower_keyword(name: str) -> str:
+    name = re.sub(r"\(\w+\s*\w+\)", "", name)
+    return name\
+        .lower()\
+        .replace(",", "")\
+        .replace("é", "e")\
+        .replace(" lighter", "")\
+        .replace(" light", "")\
+        .replace(" darker", "")\
+        .replace(" dark", "")\
+        .replace(" hard", "")\
+        .replace(" medium", "")\
+        .replace(" soft", "")\
+        .replace(" terminal", "")\
+        .replace(" high contrast", "")\
+        .replace(" contrast", "")\
+        .replace(" plus", "")\
+        .replace(" dimmed", "")\
+        .replace("  ", " ")\
+        .lstrip(" ").rstrip(" ")
 
 def convert_scheme(scheme_path: Path) -> str:
     """Convert yaml scheme to .less scheme."""
@@ -52,7 +75,7 @@ def convert_scheme(scheme_path: Path) -> str:
     if need_update:
         with less_filepath.open(mode = "w") as less_file:
             less_file.write(less_content)
-        spectrum_pixels = generate_pixels(spectrum_colors, 640, 20)
+        spectrum_pixels = generate_pixels(spectrum_colors, 720, 20)
         Image.fromarray(spectrum_pixels, mode="RGB").save(spectrum_filepath)
         print(f"Update file: {less_filename}.less")
     else:
@@ -60,22 +83,11 @@ def convert_scheme(scheme_path: Path) -> str:
 
     return yaml_content["name"].replace("-", " ").title()
 
-if __name__ == "__main__":
-    schemes = sorted(Path(source_path).glob("*.y*ml"))
+def generate_readme(name_list: list) -> str:
+    readme_content = "#Base16 Syntax Theme"
 
-    name_list = [convert_scheme(scheme) for scheme in schemes]
-
-    with Path(settings_path).open(mode = "r") as settings_file:
-        settings_content = json.loads(settings_file.read())
-
-    settings_content["config"]["scheme"]["enum"] = sorted(name_list)
-
-    with Path(settings_path).open(mode = "w") as settings_file:
-        json.dump(settings_content, settings_file, indent = 2)
-
-    schemes_markdown = ""
+    schemes_list = ""
     for name in name_list:
-    # for name in settings_content["config"]["scheme"]["enum"]:
         author = ""
         scheme_path = Path(schemes_path / PurePath(lower_filename(name)).with_suffix(".less"))
         preview_path = Path(schemes_path / PurePath(lower_filename(name)).with_suffix(".png"))
@@ -83,12 +95,28 @@ if __name__ == "__main__":
             for line in scheme_file:
                 if "Author:" in line:
                     author = line.lstrip("/").lstrip(" ").rstrip("\n").removeprefix("Author: ")
-        schemes_markdown += f"- {name} (Author: {"Unknown" if author == "" else author})\n"
+        schemes_list += f"\n- {name} (Author: {"Unknown" if author == "" else author})\n"
         if preview_path.exists():
-            schemes_markdown += f"![name](filename)\n"
+            schemes_list += f"![name](https://github.com/Digital-Punishment/base16-compilation-syntax/blob/master/styles/schemes/{lower_filename(name)}.png?raw=true)\n"
 
+    return readme_content
 
-    # pprint.pp(settings_content)
-    # print(len(name_list))
-    # pprint.pp(name_list)
-    print(schemes_markdown)
+if __name__ == "__main__":
+    schemes = sorted(Path(source_path).glob("*.y*ml"))
+
+    name_list = [convert_scheme(scheme) for scheme in schemes]
+    print(f"{len(name_list)} schemes in package")
+
+    with Path(settings_path).open(mode = "r") as settings_file:
+        settings_content = json.loads(settings_file.read())
+    settings_content["config"]["scheme"]["enum"] = sorted(name_list)
+    with Path(settings_path).open(mode = "w") as settings_file:
+        json.dump(settings_content, settings_file, indent = 2)
+
+    keywords = set([lower_keyword(name) for name in name_list])
+    keywords.add("base16")
+    with Path(package_path).open(mode = "r") as package_file:
+        package_content = json.loads(package_file.read())
+    package_content["keywords"] = sorted(keywords)
+    with Path(package_path).open(mode = "w") as package_file:
+        json.dump(package_content, package_file, indent = 2)
